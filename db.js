1530785689fa2dc7dd8d199841c5ae6be525e71d
IsabelGuMo <gm.isabel@gmail.com>
2022-12-02 17:31:04 +0100

    Create db.js file

---
+++ db.js
@@ -0,0 +1,104 @@
+const products = [
+    {
+        id:1,
+        name: 'Botas grafitty',
+        description: 'Botas de plataforma,botas góticas, botas moteras',
+        image: './assets/imagenes/shoes-photos/Botas alternativas góticas.jpg',
+        talle: 38,
+        ref: 2023,
+        color: 'black',
+        amount: 1,
+        price: 100.95
+        
+    },
+    {
+        id:2,
+        name: 'Botas hebillas',
+        description: 'Botas de plataforma,botas góticas, botas moteras, tacones altos',
+        image: './assets/imagenes/shoes-photos/Botas góticas.jpg',
+        talle: 38,
+        ref: 2022,
+        color: 'black',
+        amount: 1,
+        price: 99.95
+    },
+    {
+        id:3,
+        name: 'Botas correas',
+        description: 'Botas de plataforma,botas góticas, botas moteras',
+        image: './assets/imagenes/shoes-photos/Botin con suela lisa.jpg',
+        talle: 38,
+        ref: 2021,
+        color: 'black',
+        amount: 1,
+        price: 39.95
+    }, 
+    {
+        id:4,
+        name: 'Botin tatoo ',
+        description: 'Botín plano engomado resistente al agua. Suela dentada tipo track.',
+        image: './assets/imagenes/shoes-photos/Botin gótico.jpg',
+        talle: 38,
+        ref: 2020,
+        color: 'black',
+        amount: 1,
+        price: 129.00
+    }, 
+    {
+        id:5,
+        name: 'Botines track ',
+        description: 'Botín acharolado, suela dentada tipo track, tacón.',
+        image: './assets/imagenes/shoes-photos/Botines de suela.jpg',
+        talle: 38,
+        ref: 2019,
+        color: 'black',
+        amount: 1,
+        price: 39.95
+    },
+    {
+        id:6,
+        name: 'Botas grafitty',
+        description: 'Botas de plataforma,botas góticas, botas moteras',
+        image: './assets/imagenes/shoes-photos/Botas alternativas góticas.jpg',
+        talle: 38,
+        ref: 2018,
+        color: 'black',
+        amount: 1,
+        price: 49.95
+    },
+    {
+        id:7,
+        name: 'Botas grafitty',
+        description: 'Botas de plataforma,botas góticas, botas moteras',
+        image: './assets/imagenes/shoes-photos/Botas alternativas góticas.jpg',
+        talle: 38,
+        ref: 2017,
+        color: 'black',
+        amount: 1,
+        price: 100.95
+        
+    },
+    {
+        id:8,
+        name: 'Botas hebillas',
+        description: 'Botas de plataforma,botas góticas, botas moteras, tacones altos',
+        image: './assets/imagenes/shoes-photos/Botas góticas.jpg',
+        talle: 38,
+        ref: 2016,
+        color: 'black',
+        amount: 1,
+        price: 99.95
+    },
+    {
+        id:9,
+        name: 'Botas correas',
+        description: 'Botas de plataforma,botas góticas, botas moteras',
+        image: './assets/imagenes/shoes-photos/Botin con suela lisa.jpg',
+        talle: 38,
+        ref: 2015,
+        color: 'black',
+        amount: 1,
+        price: 39.95
+    },
+   
+];
